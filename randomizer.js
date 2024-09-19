ea6ebdcffd962e1dc43f1a178148645ed2e0c4ff
BOdiwuor <affectiveassociates@gmail.com>
2024-09-19 12:14:42 -0500

    adding file to repo

---
+++ randomizer.js
@@ -0,0 +1,29 @@
+//console.log("Let's do this!");
+
+//method #1 - using control flow
+let randomNumber = Math.floor(Math.random() * 7) + 1;
+
+let obj1 = {name : 'Nandita'}
+
+
+if (randomNumber === 1){
+    console.log ('You are a conqueror!');
+} else if (randomNumber === 2){
+    console.log ('You are doing great!');
+} else if(randomNumber === 3){
+    console.log ('The best is yet to come!');
+} else if (randomNumber === 4){
+    console.log ('20');
+}else if(randomNumber === 5){
+    console.log (obj1.name);
+} else if (randomNumber === 6){
+    console.log ('Today is your lucky day!');
+} else if (randomNumber === 7){
+    console.log ('null');
+} else {
+    console.log ('Luck is when preparation meets opportunity');
+}
+
+
+
+
